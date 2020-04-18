06db85b712ed7776f43e422bca4869b57962d088
Alberto Arroyo <Alberto.Arroyo@tranzact.net>
2020-04-18 14:06:10 -0500

    04.17.2020 adding more functional samples from knowthen

---
+++ js/functional/apps/counter/src/index.js
@@ -0,0 +1,18 @@
+import h from 'hyperscript';
+import hh from 'hyperscript-helpers';
+
+const { div, button } = hh(h);
+
+const initModel = 0;
+
+function view(model) {
+  return div([
+    div({ className: 'mv2' }, `Count: ${model}`),
+    button({ className: 'pv1 ph2' }, '+'),
+    button({ className: 'pv1 ph2' }, '-'),
+  ]);
+}
+
+const rootNode = document.getElementById('app');
+
+rootNode.appendChild(view(initModel));
