774a89828d719ceba40ceb4897633b97dc55e0fd
Alberto Arroyo <Alberto.Arroyo@tranzact.net>
2020-04-19 15:36:32 -0500

    04.19.2020 adding solution for counter

--- js/functional/apps/counter/src/index.js
+++ js/functional/apps/counter/src/index.js
@@ -3,16 +3,41 @@ import hh from 'hyperscript-helpers';
 
 const { div, button } = hh(h);
 
-const initModel = 0;
+let initModel = 0;
 
-function view(model) {
+function view(dispatch, model) {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
-    button({ className: 'pv1 ph2' }, '+'),
-    button({ className: 'pv1 ph2' }, '-'),
+    button({ className: 'pv1 ph2 mr2', onclick: () => dispatch('plus') }, '+'),
+    button({ className: 'pv1 ph2', onclick: () => dispatch('minus') }, '-'),
   ]);
 }
 
+function update(msg, model) {
+  switch (msg) {
+    case 'plus':
+      return model + 1;
+    case 'minus':
+      return model - 1;
+    default:
+      return model;
+  }
+}
+
+//Impure code below
+const app = (initModel, update, view, node) => {
+  let model = initModel;
+  let currentView = view(dispatch, model);
+  node.appendChild(currentView);
+
+  function dispatch(msg) {
+    model = update(msg, model);
+    const updatedView = view(dispatch, model);
+    node.replaceChild(updatedView, currentView);
+    currentView = updatedView;
+  }
+};
+
 const rootNode = document.getElementById('app');
 
-rootNode.appendChild(view(initModel));
+app(initModel, update, view, rootNode);
