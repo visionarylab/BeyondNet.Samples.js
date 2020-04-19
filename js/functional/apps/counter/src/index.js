8f867d95f39e770be3f4c91c8cb274a059f54e6f
Alberto Arroyo <Alberto.Arroyo@tranzact.net>
2020-04-19 15:56:39 -0500

    04.19.2020 adding solution for counter

--- js/functional/apps/counter/src/index.js
+++ js/functional/apps/counter/src/index.js
@@ -1,5 +1,6 @@
-import h from 'hyperscript';
 import hh from 'hyperscript-helpers';
+import { h, diff, patch } from 'virtual-dom';
+import createElement from 'virtual-dom/create-element';
 
 const { div, button } = hh(h);
 
@@ -8,16 +9,27 @@ let initModel = 0;
 function view(dispatch, model) {
   return div([
     div({ className: 'mv2' }, `Count: ${model}`),
-    button({ className: 'pv1 ph2 mr2', onclick: () => dispatch('plus') }, '+'),
-    button({ className: 'pv1 ph2', onclick: () => dispatch('minus') }, '-'),
+    button(
+      { className: 'pv1 ph2 mr2', onclick: () => dispatch(MSGS.ADD) },
+      '+'
+    ),
+    button(
+      { className: 'pv1 ph2', onclick: () => dispatch(MSGS.SUBSTRACT) },
+      '-'
+    ),
   ]);
 }
 
+const MSGS = {
+  ADD: 'ADD',
+  SUBSTRACT: 'SUBSTRACT',
+};
+
 function update(msg, model) {
   switch (msg) {
-    case 'plus':
+    case MSGS.ADD:
       return model + 1;
-    case 'minus':
+    case MSGS.SUBSTRACT:
       return model - 1;
     default:
       return model;
@@ -28,12 +40,15 @@ function update(msg, model) {
 const app = (initModel, update, view, node) => {
   let model = initModel;
   let currentView = view(dispatch, model);
-  node.appendChild(currentView);
+  let rootNode = createElement(currentView);
+
+  node.appendChild(rootNode);
 
   function dispatch(msg) {
     model = update(msg, model);
     const updatedView = view(dispatch, model);
-    node.replaceChild(updatedView, currentView);
+    const patches = diff(currentView, updatedView);
+    rootNode = patch(rootNode, patches);
     currentView = updatedView;
   }
 };
