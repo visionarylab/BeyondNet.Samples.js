2d27b0b6432757287f203dcadd2f59019e31df2b
Alberto Arroyo <Alberto.Arroyo@tranzact.net>
2020-05-13 13:30:09 -0500

    05.13.2020 adding new docker and docker compose samples

--- mern/server/app.js
+++ mern/server/app.js
@@ -21,8 +21,8 @@ app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
   }
-  res.status(error.code || 500)
-  res.json({message: error.message || 'An unknown error occurred!'});
+  res.status(error.code || 500);
+  res.json({ message: error.message || 'An unknown error occurred!' });
 });
 
 app.listen(5000);
